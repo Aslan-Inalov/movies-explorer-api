bcf8ceb427b8e8aeae8ab5e3a5dd3d08d24c4742
Aslan Inalov <aslaninalov@yandex.ru>
2023-06-25 19:25:42 +0300

    fix

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const router = require('./routes');
 const errorsHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./utils/limiter');
-const { PORT } = require('./config');
+const { DB_PATH, PORT } = require('./config');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect(DB_PATH);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
