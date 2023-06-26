2282486f2f5ca2a7dc71c327605fe63adfb6c452
Aslan Inalov <aslaninalov@yandex.ru>
2023-06-26 21:32:59 +0300

    fix1

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const authRouter = require('./auth');
-const auth = require('../middlewares/auth');
+// const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 
 router.use(authRouter);
-router.use('/users', auth, usersRouter);
-router.use('/movies', auth, moviesRouter);
-router.use('*', auth, (req, res, next) => {
+router.use('/users', usersRouter);
+router.use('/movies', moviesRouter);
+router.use('*', (req, res, next) => {
   next(new NotFoundError('Такой страницы не существует'));
 });
 
